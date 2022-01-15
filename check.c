405c58be0150a2273fe85069d87d8e9c0ea158fa
rchastain <roland.chastain@gmx.fr>
2022-01-15 17:29:03 +0100

    Remove unused variables

--- check.c
+++ check.c
@@ -19,56 +19,56 @@
 #include "check.h"
 
 int check(int moved_order, piece *friends, piece *enem) {
-	char fr_color, fr_type;
-	int j;
-	BITMASK king_pos;
-	int k_row, k_col;
-	
-	king_pos = enem[4].bm_pos;
-	conv_bm_cases(king_pos, &k_row, &k_col);
-	
-	fr_color = friends[0].color;
-	
-	for (j=0; j<=15; j++) {			//examine all pieces
-		if (friends[j].bm_pos == 0)           //a dead piece can do nothing
-			continue;
-		fr_type = friends[j].type;
-		
-		// r, b, q always to test (can give a discovery check)
-		if (fr_type == 'r') {
-			if (rook_eats_king(j, friends, enem, k_row, k_col) == 1) {	//a rook could always eat the king
-				return 1;
-			}
-		}
-		else if (fr_type == 'b') {
-			if (bishop_eats_king(j, friends, enem, k_row, k_col) == 1)
-				return 1;
-		}
-		else if (fr_type == 'q') {
-			if (queen_eats_king(j, friends, enem, k_row, k_col) == 1)   
-				return 1;
-		}
-		else if (fr_type == 'n') {
-			if (j != moved_order)				//if (moved_type != 'n')
-				continue;
-			if (knight_eats_king(j, friends, enem, k_row, k_col) == 1)   
-				return 1;
-		}
-		else if (fr_type == 'p') {
-			if (j != moved_order)			// not the moved pawn
-				continue;
-			if (fr_color == 'W') {
-				if (white_pawn_eats_king(j, friends, enem, king_pos) == 1)
-					return 1;
-			}
-			else {
-				if (black_pawn_eats_king(j, friends, enem, king_pos) == 1)
-					return 1;
-			}
-		}
-		else					// king cannot give check directly
-			continue;
-	}
+  char fr_color, fr_type;
+  int j;
+  BITMASK king_pos;
+  int k_row, k_col;
+  
+  king_pos = enem[4].bm_pos;
+  conv_bm_cases(king_pos, &k_row, &k_col);
+  
+  fr_color = friends[0].color;
+  
+  for (j=0; j<=15; j++) {     //examine all pieces
+    if (friends[j].bm_pos == 0)           //a dead piece can do nothing
+      continue;
+    fr_type = friends[j].type;
+    
+    // r, b, q always to test (can give a discovery check)
+    if (fr_type == 'r') {
+      if (rook_eats_king(j, friends, enem, k_row, k_col) == 1) {  //a rook could always eat the king
+        return 1;
+      }
+    }
+    else if (fr_type == 'b') {
+      if (bishop_eats_king(j, friends, enem, k_row, k_col) == 1)
+        return 1;
+    }
+    else if (fr_type == 'q') {
+      if (queen_eats_king(j, friends, enem, k_row, k_col) == 1)   
+        return 1;
+    }
+    else if (fr_type == 'n') {
+      if (j != moved_order)       //if (moved_type != 'n')
+        continue;
+      if (knight_eats_king(j, friends, enem, k_row, k_col) == 1)   
+        return 1;
+    }
+    else if (fr_type == 'p') {
+      if (j != moved_order)     // not the moved pawn
+        continue;
+      if (fr_color == 'W') {
+        if (white_pawn_eats_king(j, friends, enem, king_pos) == 1)
+          return 1;
+      }
+      else {
+        if (black_pawn_eats_king(j, friends, enem, king_pos) == 1)
+          return 1;
+      }
+    }
+    else          // king cannot give check directly
+      continue;
+  }
 
-	return 0;
+  return 0;
 }
---
+++ xboard.sh
@@ -0,0 +1,2 @@
+xboard -size small -debug -fcp ./GiuChess-1.0 -sgf games.pgn -tc 3 -inc 0 -autoflag
+
